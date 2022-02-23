d97d3a78f872fb3d3b46420169ffa8a06e19734f
Ethan Anderson <eanderson@atlassian.com>
2022-02-23 13:20:41 -0600

    regenerate for v1beta1

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-wget https://github.com/grafeas/grafeas/raw/master/proto/v1alpha1/swagger/grafeas.swagger.json -O grafeas.swagger.json
+wget https://github.com/grafeas/grafeas/raw/master/proto/v1beta1/swagger/grafeas.swagger.json -O grafeas.swagger.json
 
 wget https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.18/swagger-codegen-cli-2.4.18.jar -O swagger-codegen-cli.jar
 
